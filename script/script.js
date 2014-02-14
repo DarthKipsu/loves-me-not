19d82074b5f0753730411f8caea76d73cb6354f3
Verna Koskinen <darth.kipsu@gmail.com>
2014-02-14 22:52:47 +0200

    add drag petals

--- script/script.js
+++ script/script.js
@@ -8,6 +8,32 @@ function createPetals(amount) {
 		image.classList.add('petals');
 		image.style.transform = 'rotate(' + ( angle * ( i + 1 )) + 'rad)'
 		flower.appendChild(image);
+		image.addEventListener('dragstart', dragPetal);
+		image.addEventListener('dragend', dragendPetal);
 	};
 	flower.style.display = 'block';
+	document.body.addEventListener('drop', dropPetal);
 };
+
+var dragCount = 2;
+
+function dragPetal(event) {
+	dragCount += 1;
+	event.dataTransfer.effectAllowed = 'copy';
+	if (dragCount % 2 == 0) {
+		he_loves_me_not();
+		console.log('loves me not')
+	} else {
+		he_loves_me();
+		console.log('loves me')
+	};
+};
+
+function dragendPetal() {
+	this.style.display = 'none';
+};
+
+function dropPetal(event) {
+	event.preventDefault();
+	return false;
+}
